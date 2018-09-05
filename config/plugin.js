d475eb43f7ab42a577782d0820e18f3e60000c00
fendy <281966728@qq.com>
2018-09-05 15:57:01 +0800

    mongodb插件开发

--- config/plugin.js
+++ config/plugin.js
@@ -1,4 +1,19 @@
+const path = require('path');
+
+/**
+ * 模板引擎
+ * */
+
 exports.nunjucks = {
     enable: true,
     package: 'egg-view-nunjucks',
+};
+
+/**
+ * mongodb操作
+ * */
+
+exports.db = {
+    enable: true,
+    path: path.join(__dirname, '../lib/plugin/egg-mongodb')
 };
